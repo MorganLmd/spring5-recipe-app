8c3af197c2e949c7c7901bfe54061c18a6dbbfff
Morgan <47953421+MorganLmd@users.noreply.github.com>
2020-11-07 13:40:45 +0100

    unit testing implementation

---
+++ src/test/java/guru/springframework/domain/CategoryTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.domain;
+
+import junit.framework.TestCase;
+
+public class CategoryTest extends TestCase {
+
+    Category category;
+
+    public void setUp() {
+        this.category = new Category();
+    }
+
+    public void testGetId() {
+        Long idValue = 4L;
+
+        category.setId(idValue);
+
+        assertEquals(idValue, category.getId());
+    }
+
+    public void testGetDescription() {
+    }
+
+    public void testGetRecipes() {
+    }
+}
